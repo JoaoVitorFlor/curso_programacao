ac6fefc7fb85d3f4bf605503cd43581aeabc331c
Joao Vitor Flor da Silva <joaovitorfds95@gmail.com>
2023-06-16 22:03:21 -0300

    Exercicios da aula de Lista

---
+++ src/lista/Funcionario.java
@@ -0,0 +1,40 @@
+package lista;
+
+public class Funcionario {
+
+    private Integer id;
+    private String nome;
+    private Double salario;
+
+    public Funcionario(Integer id, String nome, Double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void aumentoSalario (double porcentagem) {
+        this.salario =  salario + (salario*porcentagem/100);
+    };
+
+
+}
